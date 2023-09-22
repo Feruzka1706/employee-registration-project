fa8321e1d14130317313f6ef5c92952beb56146b
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-09-21 21:51:20 -0500

    Employee-registration page code changes

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,8 +1,13 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,9 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
 
-    @GetMapping("/register")
-    public String createEmployee(){
+    @RequestMapping("/register")
+    public String createEmployee(Model model){
+        model.addAttribute("employee",new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
 
         return "employee/employee-create";
     }
+
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+      DataGenerator.saveEmployee(employee);
+      model.addAttribute("employees",DataGenerator.readAllEmployees());
+
+        return "employee/employee-list";
+    }
+
+
 }
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,6 +1,11 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -9,9 +14,15 @@ import lombok.*;
 @ToString
 public class Employee {
 
+    @NotNull
+    //@Pattern(regexp = "a-z")
     private String firstName;
+    @NotNull
     private String lastName;
-    private String birthday;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    //@DateTimeFormat(pattern = "MM-dd-yyyy") this format doesn't work because of DB date format usually uses as above format
+    private LocalDate birthday;
     private String email;
     private String password;
     private String address;
